3709fff277c298d8e87dd5d5daa7589eb1e8b40d
Libis Joana <libisjohana.gonzalez@gmail.com>
2021-09-10 10:02:54 -0500

    :zap: Agregando Procfile para deploy

--- server/server.js
+++ server/server.js
@@ -34,6 +34,7 @@ app.set('port', process.env.PORT || 3000);
 // app.set('views', path.join(__dirname, '/views'));
 // app.set('view engine', 'pug');
 
+
 app.use(express.static(__dirname + '/public'))
 
 
